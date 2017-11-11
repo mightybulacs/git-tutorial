0e8cc714d205e804c5c2de843d3ca2940aa9bf9c
Rommel Bulalacao <mightybulacs@gmail.com>
2017-11-11 21:17:01 +0800

    create demo for getting inputs from terminal

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+
 def multiply (a, b):
 	return a * b
 
 def add(a, b):
 	return a + b
+
+if len(sys.argv) == 3:
+	num1 = int(sys.argv[1])
+	num2 = int(sys.argv[2])
+	print(multiply(num1, num2))
+else:
+	print("invalid number of arguments")
+
